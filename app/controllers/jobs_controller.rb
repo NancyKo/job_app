f811fed15353ea634aafe7a40ec76c8f17bfce13
N K <konancys@gmail.com>
2014-11-23 23:03:09 -0500

    strong params for create job

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -8,14 +8,16 @@ def new
 end
 
 def create 
+	p "IN the create METHOD"
 	Job.create(job_params)
 	# render :index
+	p job
 	redirect_to jobs_path
 end 
 
 private 
 
-def job(params)
+def job_params
 	params.require(:job).permit(:title, :description)
 end
 end
